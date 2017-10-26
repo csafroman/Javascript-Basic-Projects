50451c6e51080ae31b9975afa08cb92390775911
Chetan Singrodia <singrodia.chetan@gmail.com>
2017-10-26 14:44:23 +0530

    added the javascript for the social media links

--- Random Quote Generator/random-quote-generator.js
+++ Random Quote Generator/random-quote-generator.js
@@ -3,13 +3,13 @@ let quotes = [
 "It is something that grows over time... A true friendship. A feeling in the heart that becomes even stronger over time.",
 "Mess with the best, you will die like the rest.",
 "We all make choices, but in the end, our choices make us.",
-"it'so much easier to be given a place to belong than to make one for yourself",
+"It's so much easier to be given a place to belong than to make one for yourself",
 "Thank you Mario! but our princess is in another castle!",
-"Experience has taught me that wishful thinking only leads to disspointment",
+"Experience has taught me that wishful thinking only leads to disappointment",
 "There is no use crying over every mistake, you just keep on trying till you run out of cake.",
 "War. War never changes.",
 "It's more important to master the cards you are holding than to complain about the ones your oponenets were dealt.",
-"In the darkets hour, there's alwyas a way out.",
+"In the darkest hour, there's always a way out.",
 "Good men mean well. We just don't always end up doing well."
 ]
 
@@ -29,8 +29,15 @@ let character = [
 "-Isaac Clarke, Dead Space 3"
 ]
 
+
 function newQuote(){
 	var randomNum = Math.floor(Math.random()*(quotes.length));
 	document.getElementById("random-quote").innerHTML = quotes[randomNum];
 	document.getElementById("character").innerHTML = character[randomNum];
+	var tweet = document.getElementById("tweet");
+	var tumblr = document.getElementById("tumblr");
+	var facebook = document.getElementById("facebook");
+	tweet.href = "http://twitter.com/intent/tweet?text=" + quotes[randomNum] + " " + character[randomNum];
+	facebook.href = "#";
+	tumblr.href = "#";
 }
